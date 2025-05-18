7f2c28201968fb911c797addb8f8e59e9f778e64
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-05-18 10:34:12 +0000

    remove this description from the card - Find relevant tuition opportunities.

--- src/app/dashboard/enquiries/page.tsx
+++ src/app/dashboard/enquiries/page.tsx
@@ -96,9 +96,9 @@ export default function AllEnquiriesPage() {
             <SearchIcon className="w-5 h-5 mr-2.5"/>
             Search & Filter Enquiries
           </CardTitle>
-          <CardDescription className="text-sm text-muted-foreground">
+          {/* <CardDescription className="text-sm text-muted-foreground">
             Find relevant tuition opportunities.
-          </CardDescription>
+          </CardDescription> */}
         </CardHeader>
         <CardContent className="flex flex-col sm:flex-row items-center gap-3">
           <div className="relative flex-grow w-full">
