f6ae9e9b91a89eaafa5c9f1739a9a702743d0cd2
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-07-28 13:34:48 +0000

    this is the sample code which sets the years of experience in ui receive

--- src/components/tutor/EditTutoringDetailsForm.tsx
+++ src/components/tutor/EditTutoringDetailsForm.tsx
@@ -144,7 +144,7 @@ export function EditTutoringDetailsForm({ onSuccess, initialData }: EditTutoring
           state: details.state,
           country: details.country,
           pincode: details.pincode,
-          googleMapsUrl: details.googleMapsLink,
+          googleMapsLink: details.googleMapsLink,
       });
       form.setValue("hourlyRate", details.hourlyRate ? String(details.hourlyRate) : "");
       form.setValue("isRateNegotiable", details.rateNegotiable || false);
