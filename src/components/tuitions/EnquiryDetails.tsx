ceb124f6a327ece72bd635c40192c2083904acee
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-07-30 11:24:23 +0000

    It was fixed in this version - b995849
    Can you refer this version to fi

--- src/components/tuitions/EnquiryDetails.tsx
+++ src/components/tuitions/EnquiryDetails.tsx
@@ -1,3 +1,4 @@
+
 // src/components/tuitions/EnquiryDetails.tsx
 "use client";
 
@@ -74,6 +75,7 @@ export function EnquiryDetails({ requirement }: EnquiryDetailsProps) {
 
   const hasScheduleInfo = (requirement.preferredDays && requirement.preferredDays.length > 0) || (requirement.preferredTimeSlots && requirement.preferredTimeSlots.length > 0);
   const hasLocationInfo = !!requirement.address && requirement.address.trim() !== '';
+
 
   const handleCopy = async (textToCopy: string, fieldName: string) => {
     if (!isApplied) {
