c02fc29e72fc3c689cc7a4d3c5d2062f36563e2e
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-08-15 08:17:36 +0000

    in mobile view when i scroll the admin layout, there is extra space at t

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -50,7 +50,7 @@ export function AdminSidebar({
         isMobile
           ? cn(
               "fixed inset-y-0 left-0 z-40 w-60 transform",
-              `top-[var(--header-height,0px)] h-[calc(100vh_-_var(--header-height,0px))]`,
+              `top-[var(--header-height)] h-[calc(100vh_-_var(--header-height))]`,
               isMobileNavOpen ? "translate-x-0" : "-translate-x-full"
             )
           : cn(
