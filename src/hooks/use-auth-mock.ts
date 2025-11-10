a73f99d34c2eda3f097ad4871f6890e38c9165c1
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-11-10 19:17:52 +0000

    when i am adding to home screen and if i am logged in, killing the homes

--- src/hooks/use-auth-mock.ts
+++ src/hooks/use-auth-mock.ts
@@ -20,7 +20,6 @@ const localStorageJSONStorage = createJSONStorage<any>(() => {
     getItem: () => null,
     setItem: () => {},
     removeItem: () => {},
-    subscribe: () => () => {},
   };
 });
 
