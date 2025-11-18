2e5970803ef04e8d1e5c1be8907c3c4fa5016f9b
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-11-18 07:47:53 +0000

    did you change header styling for terms and conditions page, it should b

--- src/app/terms-and-conditions/layout.tsx
+++
@@ -1,8 +0,0 @@
-
-import type { ReactNode } from 'react';
-import InfoPageLayout from '@/app/(info)/layout';
-
-
-export default function TermsAndConditionsLayout({ children }: { children: ReactNode }) {
-  return <InfoPageLayout>{children}</InfoPageLayout>;
-}
