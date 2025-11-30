749e5991354ec53b4edde85b16c0ccfedc71ac8a
Nitin Jhamnani <nitinjhamnani01@gmail.com>
2025-11-30 13:49:44 +0000

    can you remove redundant layouts i dont see you removing them

--- src/app/admin/feedbacks/layout.tsx
+++
@@ -1,7 +0,0 @@
-
-import type { ReactNode } from "react";
-import AdminLayout from "../layout";
-
-export default function FeedbacksLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
--- src/app/admin/queries/layout.tsx
+++
@@ -1,7 +0,0 @@
-
-import type { ReactNode } from "react";
-import AdminLayout from "../layout";
-
-export default function QueriesLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
